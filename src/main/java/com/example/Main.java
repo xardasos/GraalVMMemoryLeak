ccc4d0338e2dfd0922c84a2ebc10c150a2f729da
Tomasz Zorawik <67728999+xardasos@users.noreply.github.com>
2021-07-12 14:35:00 +0200

    Order of closing

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -22,8 +22,8 @@ public class Main {
 
             engine.eval(SCRIPT_BODY);
 
-            polyglotEngine.close();
             engine.close();
+            polyglotEngine.close();
         }
     }
 
